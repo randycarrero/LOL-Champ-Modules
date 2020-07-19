9a02bcc0bb1c214aefce0136a1e24f2a70e6422d
Randy <randycarrero9@gmail.com>
2020-07-18 20:41:34 -0400

    re eddited js and removes not used files. and

--- java.js
+++ java.js
@@ -25,19 +25,6 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }   
-//* end of Modal Code *//  
-//  Start of exporting code *//
-$("#export-file").click(() =>
-{
-
-    $("#export").click();
-
-    var blob = new Blob([$("#pre-code").val()], { type: 'text/plain' });
-    var a = document.createElement("a");
-    a.href = URL.createObjectURL(blob);
-    a.setAttribute("download", $('#project-name').val() + '.CS');
-    a.click();
-})    
-//  End of exporting code *//    
+//* end of Modal Code *//    
 });
 
