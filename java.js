3f3e783a823dd0999ed13de5057b203bf464486b
Randy <randycarrero9@gmail.com>
2020-08-04 19:48:28 -0400

    Working switch js added

--- java.js
+++ java.js
@@ -51,3 +51,11 @@ $(document).ready(function() {
   theme: "classic"
   });
 });
+
+function toggle_visibility(id) {
+  var e = document.getElementById(id);
+  if(e.style.display == 'none')
+     e.style.display = 'block';
+  else
+     e.style.display = 'none';
+}
