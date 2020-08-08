dca857b65feef2f6ebf3a0f46400761fd3502ca1
Randy <randycarrero9@gmail.com>
2020-08-07 23:47:54 -0400

    fixed slider with js addition shows value.

--- java.js
+++ java.js
@@ -60,3 +60,12 @@ function toggle_visibility(id) {
      e.style.display = 'none';
 }
 
+window.onload = ()=>{
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+}
