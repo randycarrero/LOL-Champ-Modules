64bc4fef03b27d1828eeecd50cde7af52f278770
Randy <randycarrero9@gmail.com>
2020-08-08 14:15:30 -0400

    Style Value box / will change style of sliders

--- java.js
+++ java.js
@@ -63,9 +63,10 @@ function toggle_visibility(id) {
 window.onload = ()=>{
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
+
 output.innerHTML = slider.value;
 
 slider.oninput = function() {
   output.innerHTML = this.value;
 }
-}
+  }
